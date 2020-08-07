290dd57c23c820c799688c8c85684236f3b4a22e
Brian Engert <bengert@soliantconsulting.com>
2020-08-07 12:00:28 -0500

    ADK-19 Customizable Question Text - Web
    * get questions from filemaker
    * support yes/no or number questions

--- src/Route/V1/Student.ts
+++ src/Route/V1/Student.ts
@@ -1,6 +1,4 @@
 import * as yup from 'yup';
-
-import { Numerish } from 'fm-data-api-client/lib/Layout'
 import Router from '@koa/router';
 import { client } from '../../Util/FileMaker';
 
@@ -12,15 +10,6 @@ export  type StudentFieldData = {
     Web_DOB_c: string;
     Web_DisplaySchool_c: string;
     Web_SchoolID_c: string;
-};
-
-type QuestionnaireFieldData = {
-    Q01_Fever: Numerish;
-    Q02_Sore: Numerish;
-    Q03_Nausea: Numerish;
-    Q04_Shortness: Numerish;
-    Q05_CloseContact: Numerish;
-    Q06_Temperature: Numerish;
 };
 
 router.get('/getCurrentQuestionnaire', async context => {
@@ -49,46 +38,46 @@ router.get('/getCurrentQuestionnaire', async context => {
 });
 
 const patchSchema = yup.object({
-    Q01: yup.string().required(),
-    Q02: yup.string().required(),
-    Q03: yup.string().required(),
-    Q04: yup.string().required(),
-    Q05: yup.string().required(),
-    Q06: yup.number().required(),
+    answers : yup.array().of(yup.object({
+        questionId : yup.string().required(),
+        yes : yup.boolean().required(),
+        number : yup.number()
+    }))
 });
 
-router.patch('/updateCurrentQuestionnaire', async context => {
+router.put('/updateCurrentQuestionnaire', async context => {
     const employeeID = context.request.user?.employeeID;
     const layout = client.layout('Student');
 
+    const input = await patchSchema.validate(context.request.body);
+
+    const scriptParam = {
+        "studentID": employeeID,
+        answers: input.answers.map((answer) => {
+            return {
+                ID_Question : answer.questionId,
+                Answer_yn : answer.yes ? 'Yes' : 'No',
+                Answer_Number : answer.number,
+            }
+        })
+    }
+
     const result = await layout.find({
-        Web_ID_c: `==${employeeID}`,
+        Web_ID_c : `==${employeeID}`,
     }, {
-        'script.prerequest': 'getCurrentQuestionnaire',
-        'script.prerequest.param': "{\"studentID\" : \"" + employeeID + "\"}",
+        script : 'putQuestionnaireAnswers',
+        'script.param': JSON.stringify(scriptParam)
     }, true);
 
     if (result.data.length === 0) {
-        return context.status = 204;
+        return context.status = 503;
     }
 
-    if (undefined === result['scriptResult.prerequest']) {
-        return context.status = 400; // what should this return
+    if (undefined === result['scriptResult']) {
+        return context.status = 503;
     }
 
-    const json = JSON.parse(result['scriptResult.prerequest']);
-
-    const input = await patchSchema.validate(context.request.body);
-    const questionnaireLayout = client.layout<QuestionnaireFieldData>('Questionnaire');
-    
-    await questionnaireLayout.update(json.recid, {
-        "Q01_Fever": input.Q01,
-        "Q02_Sore": input.Q02,
-        "Q03_Nausea": input.Q03,
-        "Q04_Shortness": input.Q04,
-        "Q05_CloseContact": input.Q05,
-        "Q06_Temperature": input.Q06
-    });
+    JSON.parse(result['scriptResult']);
 
     context.status = 204;
 });
